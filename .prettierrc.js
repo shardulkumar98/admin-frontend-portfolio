ad6302faf914b23b8501098aeaf2b760932b12bd
Shardul <shardul@WILASSETA0175.local>
2024-01-05 18:31:54 +0530

    fix: remove prettier ignore

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    semi: false,
-    singleQuote: true,
-    printWidth: 120,
-    trailingComma: "all",
-  };
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'all',
+}
